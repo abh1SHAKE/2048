915b5bf922cd72373b99e80464a66a55db1c9ba2
Abhishek Sharma <abhshake17@gmail.com>
2025-10-22 23:15:30 +0530

    initial setup : configure vite react app with tailwind

---
+++ 2048-game/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-2xl font-bold rubrik'>but here comes sebastian vettel</div>
+    </>
+  )
+}
+
+export default App
